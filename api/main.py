ffd1ed605d0b60680cd8271cbdeb6f73f612f907
Your Name <abirdutta90990@gmail.com>
2024-04-20 15:14:57 +0530

    Adding to vercel functions

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import logging
-from app.transcript import fetch_transcript
+from transcript import fetch_transcript
 app = FastAPI()
 
 origins = [
