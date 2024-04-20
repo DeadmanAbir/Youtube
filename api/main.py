f077af36303c2406114fa1140fa663d004a390eb
Your Name <abirdutta90990@gmail.com>
2024-04-20 19:10:41 +0530

    Fixes

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import logging
-from transcript import fetch_transcript
+# from transcript import fetch_transcript
 app = FastAPI()
 
 origins = [
@@ -28,8 +28,10 @@ class URL(BaseModel):
 @app.post("/fetch-transcript")
 async def root(body: URL):
     try:
-        response = fetch_transcript(body.url)
-        return {"transcript": response}
+        # response = fetch_transcript(body.url)
+        # return {"transcript": response}
+        return {"transcript": "yes"}
+
     except Exception as e:
         logging.error('An error occurred: %s', str(e))
         raise HTTPException(status_code=500, detail="Internal Server Error")
